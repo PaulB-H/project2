a205e87895c38a59a57ee8555049ec76d9bca659
Steven D'Arolfi <stevendarolfi@hotmail.com>
2019-11-30 15:36:06 -0500

    animation on pics, scroll to top animation on center

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "IamTheBoxGhost1971",
-    // password: "steven123",
+    // password: "IamTheBoxGhost1971",
+    password: "steven123",
     database: "fitness_hub_db"
   });
 }
